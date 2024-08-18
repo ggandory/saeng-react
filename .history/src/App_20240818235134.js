420eaee757e3252709ccfc350dc81505bc2634af
허건 <braveman0823@gmail.com>
2024-08-18 23:58:36 +0900

    props, component

--- .history/src/App_20240818235134.js
+++ .history/src/App_20240818235134.js
@@ -12,18 +12,30 @@ function Header(props) {
 }
 
 function Nav(props) {
-  const lis = [];
-  for (let i = 0; props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(
-      <li key={t.id}>
-        <a href={"/read/" + t.id}>{t.title}</a>
-      </li>
-    );
-  }
+  const lis =[
+    <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+  ]
   return (
     <nav>
-      <ol>{lis}</ol>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
     </nav>
   );
 }
@@ -39,10 +51,10 @@ function Article(e) {
 
 function App() {
   const topics = [
-    { id: 1, title: "html", body: "html is..." },
-    { id: 2, title: "css", body: "css is..." },
-    { id: 3, title: "javascript", body: "javascript is..." },
-  ];
+    {id:1, title: 'html', body: 'html is...'},
+    {id:2, title: 'css', body: 'css is...'},
+    {id:3, title: 'javascript', body: 'javascript is...'}
+  ]
   return (
     <div className="App">
       <Header title="REACT" />
